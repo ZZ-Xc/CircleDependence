a11bb117822ee4b473216359ebf7cc986e6fea63
zzxc <cuifhm0606@163.com>
2022-01-17 16:27:23 +0800

    master fourth commit

--- src/main/java/com/zzxc/GitTest.java
+++ src/main/java/com/zzxc/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello github !");
         System.out.println("hello github 3!");
         System.out.println("hello github 4!");
+        System.out.println("hello github master!");
     }
 
 
